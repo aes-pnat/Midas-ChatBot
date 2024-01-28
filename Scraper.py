3ef5f1bc8c61092071e17ca1fa9b67c8fad19abc
Ante Pasalic <apasalic@llama.novalocal>
2024-01-28 19:11:45 +0100

    dataset scraped

--- Scraper.py
+++ Scraper.py
@@ -2,9 +2,22 @@ import pickle
 import tools.prawCrypt as pc
 from getpass import getpass
 import praw
-import nltk
+from tqdm import tqdm
+import pandas as pd
 
+def retrive_submission_props(reddit, subreddits, limit=1000, time_filter='year', comment_limit=10):
+    qna_dict = {'questions' : [], 'answers' : [],'tags': []}
 
+    for s in subreddits:
+        subreddit_topic = reddit.subreddit(s)
+        print(s)
+        for submission in tqdm(subreddit_topic.top(limit=limit, time_filter=time_filter), total=limit):
+            for top_level_comment in submission.comments.list()[:comment_limit]:
+                qna_dict['questions'].append(f'{submission.title} {submission.selftext}')
+                qna_dict['answers'].append(top_level_comment.body)
+                qna_dict['tags'].append(s)
+    
+    return qna_dict
 
 def main():
     with open('./tools/encrypted_values.pkl', 'rb') as file:
@@ -23,8 +36,7 @@ def main():
         user_agent='MidasChatBot',
         username=reddit_usernName,
         password=reddit_password
-    )
-    
+        )
     subs = [
         'wallstreetbets',
         'invest',
@@ -36,12 +48,20 @@ def main():
         'cryptocurrencies'
     ]
 
-    finance_subreddits = {s : reddit.subreddit(s) for s in subs}
-    
-    for s, v in finance_subreddits.items():
-        print(s)
-        for submission in v.hot(limit=5):
-            print(submission.title)
+    test_subreddit = reddit.subreddit('wallstreetbets')
+
+    for submission in test_subreddit.hot(limit=1):
+        print(submission.title)
+        print(submission.selftext)
+        print(submission.comments)
+        print(submission.comments.list())
+
+
+    qna = retrive_submission_props(reddit, subs, limit=1000, time_filter='year', comment_limit=10)
+    qna_df=pd.DataFrame(qna)
+    qna_df.to_csv('qna_df_reddit.csv',index=False)
+
+
     
 
 if __name__ == '__main__':
